394682691ecd4ca10adf2c1e105dd804ed8acbb5
Tanakorn Tampanya <ducth_miw@hotmail.com>
2020-06-04 15:06:12 +0700

    double scrollbar

--- src/views/models/Createmodels/index.js
+++ src/views/models/Createmodels/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect }  from 'react';
 import Grid from '@material-ui/core/Grid';
-import PerfectScrollbar from 'react-perfect-scrollbar';
+import ScrollBar  from 'react-perfect-scrollbar';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
@@ -21,16 +21,20 @@ const useStyles = makeStyles(theme=>({
     minWidth: 100,
     marginTop : 400,
   },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+  container :{
+    display: 'flex',
+    flexDirection: 'row',
+    height: '90vh',
   },
-  title: {
-    fontSize: 14,
+  leftCol :{
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  pos: {
-    marginBottom: 12,
+  centerCol :{
+    flex: 1,
+    overflowY: 'scroll',
   },
   wrapper: {
     display: 'flex',
@@ -41,71 +45,46 @@ const useStyles = makeStyles(theme=>({
       paddingLeft: 256
     }
   },
+  example: {
+    width: '400px',
+    height: '400px'
+  },
+  econtent: {
+      width: 800,
+      height: 480,
+  },
+  
 }));
 
 function Createmodels() {
   const classes = useStyles()
+  const items = [...Array(100)].map((val, i) => `Item ${i}`);
+  function logEvent(type) {
+    console.log(`event '${type}' triggered!`);
+  }
   return (
-    <div>
-    <Box
-      height="100%"
-      display="flex"
-      flexDirection="row"
-      flexDirection="row"
-      overflow= 'hidden'
-
-    >
-       <Grid
-        className={classes.wrapper}
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
+    <div className={classes.container}>
+    
+      <Grid 
+        className={classes.centerCol}
         lg={7}
         sm={7}
-        xs={7}
-      >
-        <PerfectScrollbar options={{ suppressScrollX: true }}>
-          <div>
-        <Card >
-          <CardContent className={classes.content}>
-            1
-          </CardContent>
-        </Card>
-        <Card >
-          <CardContent className={classes.content}>
-            1
-          </CardContent>
-        </Card>
-        </div>
-        </PerfectScrollbar>
+        xs={7}>
+        <span>List</span>
+        <ul>
+          {items.map((item, i) => (<li key={`item_${i}`}>{ item }</li>))}
+        </ul>
       </Grid>
-      
-      <Grid
-        className={classes.wrapper}
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
+      <Grid className={classes.centerCol}
         lg={5}
         sm={5}
-        xs={5}
-        
-        
-      >
-        <Card >
-          <CardContent className={classes.content}>
-            2
-          </CardContent>
-        </Card>
-        <Card >
-          <CardContent className={classes.content}>
-            2
-          </CardContent>
-        </Card>
+        xs={5}>
+        <span>List</span>
+        <ul>
+          {items.map((item, i) => (<li key={`item_${i}`}>{ item }</li>))}
+        </ul>
       </Grid>
       
-    </Box>
     </div>
   )
 }
